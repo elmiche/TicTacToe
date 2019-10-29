2847c410d128dd1f95f705ae7e0282448af31aab
MichéLozano <elmiche@latinooutdoors.org>
2019-10-28 19:36:29 -0700

    before while loop

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,7 +8,9 @@ class Player:
     def __repr__(self):
         return f'Player {self.num}\'s name is {self.name} and their token is {self.token}'
 
+#needs move function
 class Game:
+
     def __init__(self):
         self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
     
@@ -21,13 +23,34 @@ class Game:
                 if j < 2:
                     row += "|"
             board += f"{row}\n"
-
         return board
+    
+    def move(self, row, column, token):
+        if self.board[row][column] != ' ':
+            return False
+        else:
+            self.board[row][column] = token
+            return True
+
+    def is_full(self):
+        full = True
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] == ' ':
+                    full = False
+                    return full
+                else:
+                    continue
+        return full
+
+game = Game()
+print(game.is_full())
+
 
 def main():
-    board = Game()
+    game = Game()
 
-    print(board)
+    print(game)
 
     
     name1 = input('What is Player 1\'s name?: ')
@@ -35,14 +58,29 @@ def main():
     name2 = input('What is Player 2\'s name?: ')
     token2 = 'O'
 
+    player_1 = Player(name1, token1, 1)
+    player_2 = Player(name2, token2, 2)
 
 
-    player_1 = Player(name1, token1, '1')
-    player_2 = Player(name2, token2, '2')
-   
     print(player_1)
     print(player_2)
-    
+
+#TODO print position on board
+    x = int(input('enter x:'))
+    y = int(input('enter y:'))
+#replace with token
+#
+    valid_move = game.move(x, y, player_1.token) # think self.token at player_1.token
+    if valid_move: 
+        print(game)
+    else:
+        print('Invalid Move')
+
+
+
+
+
+
 main()
     
 
